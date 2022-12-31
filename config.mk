fa968bcfa87c5eeea84cb9fc58df279d0a889ec0
Joker-V2 <nipinna0@gmail.com>
2022-12-31 23:17:43 +0700

    MiuiCamera: Update MiuiCamera to v130
    - Complete switch over to MiuiCamera
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- config.mk
+++ config.mk
@@ -12,22 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VENDOR_PATH := vendor/ANXCamera
+VENDOR_PATH := vendor/MiuiCamera
 
 RELAX_USES_LIBRARY_CHECK := true
 
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/ANXCamera/lib/arm,system/priv-app/ANXCamera/lib/arm) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/ANXCamera/lib/arm64,system/priv-app/ANXCamera/lib/arm64) \
-        $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/bin,system/bin) \
-        $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib,system/lib) \
-        $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64)
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiCamera/lib/arm64,system/priv-app/MiuiCamera/lib/arm64) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiExtraPhoto/lib/arm64,system/priv-app/MiuiExtraPhoto/lib/arm64) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64)
 
 PRODUCT_PACKAGES += \
-    ANXCamera \
-    ANXCameraOverlay
+    MiuiCamera \
+    MiuiCameraOverlay \
+    MiuiExtraPhoto
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.vendor.camera.enableAdvanceFeatures=0x3E7 \
+    persist.vendor.camera.multicam=TRUE \
+    persist.vendor.camera.multicam.fpsmatch=TRUE \
+    persist.vendor.camera.multicam.framesync=1 \
+    persist.vendor.camera.multicam.hwsync=TRUE \
+    persist.vendor.camera.privapp.list=com.android.camera \
+    persist.vendor.camera.picturesize.limit.enable=false \
+    ro.miui.notch=1 \
+    ro.product.device=violet \
+    ro.miui.ui.version.code=13 \
+    ro.miui.ui.version.name=V130 \
+    ro.miui.region=CN \
+    ro.miui.build.region=cn \
+    ro.fota.oem=Xiaomi \
+    ro.boot.camera.config=_pro \
+    ro.com.google.lens.oem_camera_package=com.android.camera \
+    ro.control_privapp_permissions=log
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/ANXCamera
+    vendor/MiuiCamera
